a9472c43fd612dd71f235d63d50fa3c4bf9da6d4
Tinotenda Chemvura <ttchemvura@gmail.com>
2017-01-04 21:08:03 +0200

    finished v1.0

--- src/CrackWindow.Designer.cs
+++ src/CrackWindow.Designer.cs
@@ -79,7 +79,7 @@
             this.logo.TabStop = false;
             this.logo.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // Form1
+            // CrackForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -89,7 +89,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.crack_btn);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.Name = "CrackForm";
             this.Text = "EOS Utility Crack";
             ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
             this.ResumeLayout(false);
