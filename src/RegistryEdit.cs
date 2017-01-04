28ac020b98d751210b9b23d688ddf39b12d56174
Tinotenda Chemvura <ttchemvura@gmail.com>
2017-01-04 19:42:30 +0200

    Modified the program to add a keys everywhere!

--- src/RegistryEdit.cs
+++ src/RegistryEdit.cs
@@ -13,8 +13,14 @@ namespace WindowsFormsApplication1.src
         {
             try
             {
-                RegistryKey localMachineKey, softwareKey, canonKey, eos;
+                RegistryKey localMachineKey;
+                // Add the registry Key
+                // Write to both locations so that it doesn't matter whether the machine is 32bit or 64bit
                 localMachineKey = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\\Canon\\EOS Utility");
+                localMachineKey = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\\WOW6432Node\\Canon\\EOS Utility");
+
+                // check to see if the key exists
+
             }
             catch (Exception ex)
             {
